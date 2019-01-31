4c218bcf63768ce4e7c5efcdd7bb1db68be81b4c
Faizah Aulia R <zahaulia@gmail.com>
2019-01-31 09:47:23 +0700

    Caesar Cipher
    Caesar Cipher code for learning in the odin project

---
+++ caesar_cipher.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(sentence, range)
+
+	sentence = sentence
+	range = range
+	space_ascii = " ".bytes
+
+	result = sentence.each_byte do |value|
+
+
+		if value == space_ascii
+			print value.chr
+			else
+			print (value+range).chr
+		end
+	end
+end
